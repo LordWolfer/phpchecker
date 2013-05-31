04570c514a248cdb2b7cc331b59c7964cf518b91
LordWolfer <wolfer@lavabit.com>
2013-05-31 13:16:11 +0200

    Bugfix.

--- phpchecker.sh
+++ phpchecker.sh
@@ -65,61 +65,61 @@ fcheck(){
 	do
 		#php.ini overrides....
 		if [[ "$l" == *"ini_set"* ]] ; then
-			echo -e $RED"$i seems to override our php.ini settings! "
+			echo -e $RED"$1 seems to override our php.ini settings! "
 		fi
 		if [[ "$l" == *"error_reporting"* ]] || [[ "$l" == *"error_log"* ]] ; then
-			echo -e $RED"$i seems to tamper with the error reporting! "
+			echo -e $RED"$1 seems to tamper with the error reporting! "
 		fi		
 		if [[ "$l" == *"display_error"* ]] ; then
-			echo -e $RED"$i seems to set the displaying of errors! "
+			echo -e $RED"$1 seems to set the displaying of errors! "
 		fi
 		#running code at runtime....
 		if [[ "$l" == *"eval"* ]] ; then
-			echo -e $RED"$i seems to be calling eval on something! "
+			echo -e $RED"$1 seems to be calling eval on something! "
 		fi
 		if [[ "$l" == *"exec"* ]] ; then
-			echo -e $RED"$i seems to be running a shell code! "
+			echo -e $RED"$1 seems to be running a shell code! "
 		fi
 		#checking obfu...
 		if [[ "$l" == *"base64_decode"* ]] ; then
-			echo -e $YELLOW"$i seems to be decoding something! "
+			echo -e $YELLOW"$1 seems to be decoding something! "
 		fi
 		if [[ "$l" == *"gzinflate"* ]] ; then
-			echo -e $YELLOW"$i seems to extract something gzipped! "
+			echo -e $YELLOW"$1 seems to extract something gzipped! "
 		fi
 		#checking if it modifyes the file system
 		if [[ "$l" == *"fwrite"* ]] ; then
-			echo -e $YELLOW"$i tries to write into a file! "
+			echo -e $YELLOW"$1 tries to write into a file! "
 		fi
 		#checking XSS
 		if [[ "$l" == *[I,i][F,f][R,r][A,a][M,m][E,e]* ]] ; then
-			echo -e $YELLOW"$i contains an iframe! "
+			echo -e $YELLOW"$1 contains an iframe! "
 		fi
 		if [[ "$l" == *"redsirenwebsolutions.com"* ]] || [[  "$l" == *"ha.ckers.org"* ]] ; then
-			echo -e $RED"$i mentions a bad domain! "
+			echo -e $RED"$1 mentions a bad domain! "
 		fi
 		if [[ "$l" == *"fromCharCode"* ]] ; then
-			echo -e $RED"$i contains obfuscated javascript! "
+			echo -e $RED"$1 contains obfuscated javascript! "
 		fi
 		if [[ "$l" == *"<"[I,i][M,m][G,g]" "[S,s][R,r][C,c]"="[\",\'][J,j][A,a][V,v][A,a][S,s][C,c][R,r][I,i][P,p][T,t]":"* ]] || [[  "$l" == *"<"[I,i][M,m][G,g]" "[S,s][R,r][C,c]"="[J,j][A,a][V,v][A,a][S,s][C,c][R,r][I,i][P,p][T,t]* ]] ; then
-			echo -e $RED"$i contains a javascript hidden as an image! "
+			echo -e $RED"$1 contains a javascript hidden as an image! "
 		fi
 		if [[ "$l" == *"document.cookie"* ]] ; then
-			echo -e $RED"$i plays with the user's cookie! "
+			echo -e $RED"$1 plays with the user's cookie! "
 		fi
 		if [[ "$l" == *"&#"[0-9]* ]] || [[ "$l" == *"&#x"[0-9]* ]] ; then
-			echo -e $RED"$i contains someting obfuscated (utf-8 encoded)! "
+			echo -e $RED"$1 contains someting obfuscated (utf-8 encoded)! "
 		fi
 		#checking if it sends raw data to the browser
 		if [[ "$BSCHECK" == "true" ]] && [[ "$l" == *"passthru"* ]] ; then
-			echo -e $YELLOW"$i seems to be opening a byte-stream to the browser! "
+			echo -e $YELLOW"$1 seems to be opening a byte-stream to the browser! "
 		fi
 		#additional checks....
 		if [[ "$ADDCHK" == "true" ]] ; then
 			for j in $(cat $ADDLIST)
 			do
 				if [[ "$l" == *"$j"* ]] ; then
-					echo -e $GREEN"$i valideted true when checking $j from an user supplied list! "
+					echo -e $GREEN"$1 valideted true when checking $j from an user supplied list! "
 				fi
 			done
 		fi
